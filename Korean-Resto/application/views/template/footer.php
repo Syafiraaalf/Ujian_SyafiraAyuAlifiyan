dee9118e74e59024887e20803407ed94c5d7e0cb
syafiraaalf <syafiraalifiyan@gmail.com>
2022-01-06 23:04:21 +0700

    first

---
+++ Korean-Resto/application/views/template/footer.php
@@ -0,0 +1,12 @@
+<?php
+$meta = $this->menu_m->select_meta()->row();
+?>
+<div class="page-footer">
+	<div class="page-footer-inner">
+		<?=date('Y'); ?> &copy; <?=$meta->meta_name;?> | by <a href='Syafira Ayu Alifiyan' title='Syafira Ayu Alifiyan' target='_blank'>Syafira Ayu Alifiyan</a>
+		
+	</div>
+	<div class="scroll-to-top">
+	    <i class="icon-arrow-up"></i>
+	</div>
+</div>
